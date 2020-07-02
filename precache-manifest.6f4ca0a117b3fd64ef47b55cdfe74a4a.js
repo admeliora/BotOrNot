cfe5e9f4b505d4c809917409703f7e058147f30d
Emily Yin <yin.emily@yahoo.com>
2020-07-02 18:17:00 -0400

    Updates

---
+++ precache-manifest.6f4ca0a117b3fd64ef47b55cdfe74a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3579fd25a12b436aae476c1906ff88",
+    "url": "/BotOrNot/index.html"
+  },
+  {
+    "revision": "81fc6db6e43ece3b422e",
+    "url": "/BotOrNot/static/css/main.f7feef09.chunk.css"
+  },
+  {
+    "revision": "5bb06318c601c49f0b54",
+    "url": "/BotOrNot/static/js/2.452eb71c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BotOrNot/static/js/2.452eb71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81fc6db6e43ece3b422e",
+    "url": "/BotOrNot/static/js/main.fe3bf2e9.chunk.js"
+  },
+  {
+    "revision": "dfe34c0a70ed5b2131e7",
+    "url": "/BotOrNot/static/js/runtime-main.4dcc6883.js"
+  }
+]);
